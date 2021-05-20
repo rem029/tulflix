f7d087a669a625c76e1ebe740183676d55a69223
rem029 <elawrenceponce@gmail.com>
2021-05-20 18:13:36 +0300

    added channel activity

--- src/helpers/ytAPI.js
+++ src/helpers/ytAPI.js
@@ -4,11 +4,20 @@ const apiKEY = process.env.REACT_APP_KEY;
 const chId = "UCxhygwqQ1ZMoBGQM2yEcNug"; //TULFO CHANNEL ID
 
 const ytAPI = {
-  GetChannelInfo: (channelId = chId) => useYoutubeAPI(type.channel, apiKEY, channelId),
+  GetChannelInfo: (channelId = chId) =>
+    useYoutubeAPI(type.channel, apiKEY, channelId),
 
-  GetPlaylists: (channelId = chId) => useYoutubeAPI(type.playlists, apiKEY, channelId),
+  GetChannelActivity: (channelId = chId) =>
+    useYoutubeAPI(type.channelActivity, apiKEY, channelId),
 
-  GetPlaylistItems: (playlistId) => useYoutubeAPI(type.playlist_item, apiKEY, playlistId),
+  GetChannelVideos: (channelId = chId) =>
+    useYoutubeAPI(type.channelVideos, apiKEY, channelId),
+
+  GetPlaylists: (channelId = chId) =>
+    useYoutubeAPI(type.playlists, apiKEY, channelId),
+
+  GetPlaylistItems: (playlistId) =>
+    useYoutubeAPI(type.playlist_item, apiKEY, playlistId),
 
   GetVideoInfo: (videoId) => useYoutubeAPI(type.video_info, apiKEY, videoId),
 };
--- src/services/useYoutubeAPI.js
+++ src/services/useYoutubeAPI.js
@@ -5,16 +5,25 @@ export const type = {
   channel: "channel",
   playlists: "playlists",
   playlist_item: "playlist_item",
-  video_info: "video_info",
+  videoInfo: "videoInfo",
+  channelActivity: "channelActivity",
+  channelVideos: "channelVideos",
 };
 
-const useYoutubeAPI = (type, key, id = "0", maxResults = 50) => {
+const useYoutubeAPI = (
+  type,
+  key,
+  id = "0",
+  maxResults = 50,
+  pageToken = ""
+) => {
   const urls = {
     channel: `https://youtube.googleapis.com/youtube/v3/channels?part=snippet&part=statistics&part=contentDetails&part=topicDetails&id=${id}&maxResults=${maxResults}&key=${key}`,
     playlists: `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&channelId=${id}&maxResults=${maxResults}&key=${key}`,
     playlist_item: ` https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&part=id&part=contentDetails&part=status&playlistId=${id}&maxResults=${maxResults}&key=${key}`,
-
-    video_info: `https://youtube.googleapis.com/youtube/v3/videos?id=${id}&part=snippet&part=statistics&maxResults=${maxResults}&key=${key}`,
+    videoInfo: `https://youtube.googleapis.com/youtube/v3/videos?id=${id}&part=snippet&part=statistics&maxResults=${maxResults}&key=${key}`,
+    channelActivity: `https://youtube.googleapis.com/youtube/v3/activities?part=contentDetails&part=snippet&channelId=${id}&maxResults=${maxResults}&key=${key}`,
+    channelVideos: "",
   };
 
   const [result, setResult] = useState(null);
